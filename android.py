267ec9423b3db99532e338aee79d61075abea855
nittyan <kaziotore@gmail.com>
2013-06-19 23:42:24 +0900

    pep8 pyflakes

--- android.py
+++ android.py
@@ -8,46 +8,56 @@ import codecs
 import sys
 import os
 
+
 urls = {
-	'top_paid_in_games': 'https://play.google.com/store/apps/category/GAME/collection/topselling_paid?start=%(start)d&num=24',
-	"top_free_in_games": 'https://play.google.com/store/apps/category/GAME/collection/topselling_free?start=%(start)d&num=24'
+    'top_paid_in_games':
+    ('https://play.google.com/store/apps/'
+        'category/GAME/collection/topselling_paid?start=%(start)d&num=24'),
+    "top_free_in_games":
+    ('https://play.google.com/store/apps/'
+        'category/GAME/collection/topselling_free?start=%(start)d&num=24')
 }
 rank = 1
 
 d = datetime.datetime.today()
 date_str = d.strftime('%Y%m%d%H%M')
 
+
 def get():
-	for key in urls.keys():
-		parse_rank(urls[key], key)
+    for key in urls.keys():
+        parse_rank(urls[key], key)
+
 
 def parse_rank(url, name):
-	file_name = name + '_' + date_str + '.txt'
-	file_path = os.path.join(os.path.dirname(__file__), 'dist', 'android', file_name)
-	file = codecs.open(file_path, 'w', 'utf-8')
-	print('parse ' + name + " start...")
-	for i in range(20):
-		sys.stdout.write('\r page %d parsing...' %(i))
-		response = urllib2.urlopen(url % {'start': i * 24})
-		html = response.read()
-		parse_page(file, html)
+    file_name = name + '_' + date_str + '.txt'
+    file_path = os.path.join(
+        os.path.dirname(__file__), 'dist', 'android', file_name)
+    file = codecs.open(file_path, 'w', 'utf-8')
+    print('parse ' + name + " start...")
+    for i in range(20):
+        sys.stdout.write('\r page %d parsing...' % (i))
+        response = urllib2.urlopen(url % {'start': i * 24})
+        html = response.read()
+        parse_page(file, html)
 
-	print("")
-	file.close()
-	__main__.rank = 1
+    print("")
+    file.close()
+    __main__.rank = 1
+
 
 def parse_page(file, html):
-	soup = BeautifulSoup(html)
-	aTags = soup.findAll('a')
-	for tag in aTags:
-		if 'class' in tag.attrs:
-			if 'title' == tag['class'][0]:
-				try:
-					file.write('%d,%s' % (__main__.rank, tag['title']))
-				except UnicodeEncodeError:
-					file.write('%d,%s' % (__main__.rank, 'Encode Error'))
-				file.write('\n')
-				__main__.rank += 1
+    soup = BeautifulSoup(html)
+    aTags = soup.findAll('a')
+    for tag in aTags:
+        if 'class' in tag.attrs:
+            if 'title' == tag['class'][0]:
+                try:
+                    file.write('%d,%s' % (__main__.rank, tag['title']))
+                except UnicodeEncodeError:
+                    file.write('%d,%s' % (__main__.rank, 'Encode Error'))
+                file.write('\n')
+                __main__.rank += 1
+
 
 if __name__ == '__main__':
-	get()
+    get()
--- ios.py
+++ ios.py
@@ -7,36 +7,46 @@ import codecs
 import os
 import __main__
 
+
 d = datetime.datetime.today()
 date_str = d.strftime('%Y%m%d%H%M')
 rank = 1
 
 urls = {
-	'iTunes_Store_Games_Top_Free': 'http://itunes.apple.com/jp/rss/topfreeapplications/limit=300/genre=6014/xml',
-	'iTunes_Store_Games_Top_Paid': 'http://itunes.apple.com/jp/rss/toppaidapplications/limit=300/genre=6014/xml',
-	'iTunes Store Top Sales': 'http://itunes.apple.com/jp/rss/topgrossingapplications/limit=300/genre=6014/xml'
+    'iTunes_Store_Games_Top_Free':
+    ('http://itunes.apple.com/jp/rss/'
+        'topfreeapplications/limit=300/genre=6014/xml'),
+    'iTunes_Store_Games_Top_Paid':
+    ('http://itunes.apple.com/jp/rss/'
+        'toppaidapplications/limit=300/genre=6014/xml'),
+    'iTunes Store Top Sales':
+    ('http://itunes.apple.com/jp/rss/'
+        'topgrossingapplications/limit=300/genre=6014/xml')
 }
 
+
 def get():
-	for key in urls:
-		parse_rank(urls[key], key)
+    for key in urls:
+        parse_rank(urls[key], key)
+
 
 def parse_rank(url, name):
-	print('parse ' + name + ' start...')
-	response = urllib2.urlopen(url)
-	soup = BeautifulSoup(response.read())
-	entries = soup.findAll('entry')
-	file_name = name + "_" + date_str + ".txt"
-	file_path = os.path.join(os.path.dirname(__file__), 'dist', 'ios', file_name)
-	file = codecs.open(file_path, 'w', 'utf-8')
-	for entry in entries:
-		file.write('%d,%s' %(__main__.rank, entry.find('title').text))
-		file.write('\n')
-		__main__.rank += 1
+    print('parse ' + name + ' start...')
+    response = urllib2.urlopen(url)
+    soup = BeautifulSoup(response.read())
+    entries = soup.findAll('entry')
+    file_name = name + "_" + date_str + ".txt"
+    file_path = os.path.join(
+        os.path.dirname(__file__), 'dist', 'ios', file_name)
+    file = codecs.open(file_path, 'w', 'utf-8')
+    for entry in entries:
+        file.write('%d,%s' % (__main__.rank, entry.find('title').text))
+        file.write('\n')
+        __main__.rank += 1
 
-	file.close()
-	__main__.rank = 1
+    file.close()
+    __main__.rank = 1
 
 
 if __name__ == "__main__":
-	get()
+    get()
